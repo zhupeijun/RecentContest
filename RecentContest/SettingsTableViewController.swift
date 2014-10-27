a3cd9abf1ceeed15bf08fd116adc72a11282ade6
zhu_peijun <peijun.zhu@drecom.co.jp>
2014-10-27 23:53:07 +0900

    add Settings View Controller

---
+++ RecentContest/SettingsTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsTableViewController.swift
+//  RecentContest
+//
+//  Created by zhu peijun on 10/27/14.
+//  Copyright (c) 2014 my. All rights reserved.
+//
+
+import UIKit
+
+let kTimeSettingRow = 0
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return super.numberOfSectionsInTableView(tableView)
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return super.tableView(tableView, numberOfRowsInSection: section)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var row = indexPath.row
+        
+        if(row == kTimeSettingRow)  {
+        }
+    }
+}
